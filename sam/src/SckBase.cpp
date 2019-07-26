cd489c73dcafe82b9890508b213fab1e17603442
Victor Barberan <victor@smartcitizen.me>
2019-07-26 11:16:19 +0200

    Fix SAM build error

--- sam/src/SckBase.cpp
+++ sam/src/SckBase.cpp
@@ -1984,7 +1984,8 @@ bool SckBase::setTime(String epoch)
 {
 	uint32_t wasOn = rtc.getEpoch() - espStarted;
 	rtc.setEpoch(epoch.toInt());
-	if (abs(rtc.getEpoch() - epoch.toInt()) < 2) {
+	int32_t diff = rtc.getEpoch() - epoch.toInt();
+	if (abs(diff) < 2) {
 		timeSyncAfterBoot = true;
 		st.timeStat.setOk();
 		if (urbanPresent) {
