26aa4ea00735cf3f21e65197881ec616de2ddb76
Victor Barberan <victor@smartcitizen.me>
2019-08-21 19:32:51 +0200

    Fix kit not waking up on button click

--- sam/src/SckBase.cpp
+++ sam/src/SckBase.cpp
@@ -1354,9 +1354,6 @@ void SckBase::goToSleep()
 	// Stop PM sensor
 	if (urban.sck_pm.started) urban.sck_pm.stop();
 
-	// Turn off USB led
-	digitalWrite(pinLED_USB, HIGH);
-
 	if (sckOFF) {
 
 		sprintf(outBuff, "Sleeping forever!!! (until a button click)");
@@ -1369,9 +1366,8 @@ void SckBase::goToSleep()
 		// Detach sdcard interrupt to avoid spurious wakeup
 		detachInterrupt(pinCARD_DETECT);
 
-		// Atach button wake Up interrupt
-		pinMode(pinBUTTON, INPUT_PULLUP);
-		LowPower.attachInterruptWakeup(pinBUTTON, ISR_button, CHANGE);
+		// Turn off USB led
+		digitalWrite(pinLED_USB, HIGH);
 
 		LowPower.deepSleep();
 	} else {
@@ -1380,6 +1376,9 @@ void SckBase::goToSleep()
 		sckOut();
 
 		st.sleeping = true;
+
+		// Turn off USB led
+		digitalWrite(pinLED_USB, HIGH);
 
 		LowPower.deepSleep(sleepTime);
 	}
